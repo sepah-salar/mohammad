f8b0e58a4fa258d67d8607a2793bf9a964513ecd
Mohammad99 <mohammad1386mohammad720@gmail.com>
2024-11-10 13:58:11 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include("hed.html");
+?>
+    <div id="chap">
+        <span id="vasat1">راست</span>
+    </div>
+    <div id="rast">
+        <span id="vasat2">چپ</span>
+    </div>
+<?php
+include("fot.html");
+?>
